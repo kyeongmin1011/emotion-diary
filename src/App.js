a78455af563c5c4840eebd3276fcb9591792ec61
kyeongmin1011 <kyeongmin@safety-as.com>
2022-11-24 13:15:38 +0900

    프로젝트 기초공사 1
    -폰트 세팅
    -레이아웃 세팅
    -이미지 에셋 세팅
    -공통 컴포넌트 세팅

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-
 import Home from 'pages/Home';
 import New from 'pages/New';
 import Diary from 'pages/Diary';
 import Edit from 'pages/Edit';
 import NotFound from 'pages/NotFound';
-
-import RouteTest from "./components/RouteTest";
-
+import MyButton from "components/MyButton";
+import MyHeader from "components/MyHeader";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h2>app</h2>
-        <RouteTest />
+        <MyHeader leftChild={<MyButton text={'<'} />}
+                  headText={'app'}
+                  rightChild={<MyButton text={'>'} />}/>
 
         <Routes>
           <Route path={'/'}      element={ <Home /> } />
@@ -24,6 +23,10 @@ function App() {
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
           <Route path={'*'}  element={ <NotFound /> } />
         </Routes>
+
+        <MyButton text={'버튼'} type={'positive'} onClick={() => alert('버튼 클릭')} />
+        <MyButton text={'버튼'} type={'negative'} onClick={() => alert('버튼 클릭')} />
+        <MyButton text={'버튼'} onClick={() => alert('버튼 클릭')} />
       </div>
     </BrowserRouter>
   );
--- src/components/RouteTest.js
+++ src/components/RouteTest.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 const RouteTest = () => {
   return (
     <div className="RouteTest">
-      <Link className="link" to={'/'} >Home</Link>
+      <Link className="link" to={'/'} >Home 홈페이지</Link>
       <Link className="link" to={'/diary'} >Diary</Link>
       <Link className="link" to={'/new'} >New</Link>
       <Link className="link" to={'/edit'} >Edit</Link>
