2f54fbbfa51a42f31fbc7f22a573dc9a12f1ed0e
kyeongmin1011 <kyeongmin@safety-as.com>
2022-11-30 09:45:38 +0900

    init

--- src/pages/NotFound.js
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return (
-    <div className="NotFound">
-      <h2>Not Found</h2>
-    </div>
-  )
-}
-
-export default NotFound
--- src/util/data.js
+++
@@ -1,8 +0,0 @@
-export const getstringDate = (date) => {
-  let year = date.getFullYear();
-  let month = date.getMonth() + 1;
-  let day = date.getDate();
-  if (month < 10) month = `0${month}`;
-  if (day < 10) day = `0${day}`;
-  return `${year}-${month}-${day}`;
-}
